55eafb18328a7c45133303b59a6ad988fc51740b
unknown <theto1172>
2022-12-21 08:17:40 +0630

    my test

---
+++ week 1/jQuery Plugins & Flexslider/myscript.js
@@ -0,0 +1,9 @@
+  $(window).on('load',function() {
+    $('.flexslider').flexslider({
+      animation: "slide",
+      slideshowSpeed: 2000,
+      direction: "vertical",
+      reverse: true,
+      pauseOnHover: true   
+    });
+  });
